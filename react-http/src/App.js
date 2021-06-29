7adde1b84b77415c24233ea813b7910391bcadec
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2021-06-29 20:01:11 +0530

    useEffect

--- react-http/src/App.js
+++ react-http/src/App.js
@@ -5,12 +5,13 @@ import HookCounter from './components/use-state-hook/HookCounter';
 import HookCounterTwo from './components/use-state-previous-state/HookCounterTwo';
 import HookCounterTree from './components/use-state-object/HookCounterThree';
 import HookCounterFour from './components/use-state-array/HookCounterFour';
+import HookCounterOne from './components/use-effect/HookCounterOne';
 
 
 function App() {
   return (
     <div>
-      <HookCounterFour />
+      <HookCounterOne />
     </div>
   );
 }
