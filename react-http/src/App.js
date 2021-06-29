8020dd37bc7ba10134e8ccaff05bc33f53d06a54
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2021-06-29 18:29:27 +0530

    useState Hook

--- react-http/src/App.js
+++ react-http/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PostList from './components/get-request/PostList'
 import PostForm from './components/post-request/PostForm';
+import HookCounter from './components/use-state-hook/HookCounter';
 
 function App() {
   return (
     <div>
-      <PostForm />
+      <HookCounter />
     </div>
   );
 }
