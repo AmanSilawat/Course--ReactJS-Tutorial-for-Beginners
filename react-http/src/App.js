bb8b71c8babaf09e5b2185d2dff3ff2ae866fac2
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2021-06-29 18:45:30 +0530

    useState Hook previous value

--- react-http/src/App.js
+++ react-http/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PostList from './components/get-request/PostList'
 import PostForm from './components/post-request/PostForm';
 import HookCounter from './components/use-state-hook/HookCounter';
+import HookCounterTwo from './components/use-state-previous-state/HookCounterTwo';
 
 function App() {
   return (
     <div>
-      <HookCounter />
+      <HookCounterTwo />
     </div>
   );
 }
