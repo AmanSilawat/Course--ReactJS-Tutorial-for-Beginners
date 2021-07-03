8ec89a22962126f3371b2d77a42d17cd710547ec
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2021-07-03 19:35:11 +0530

    useContext

--- react-http/src/components/use-context/ex/ComponentF.js
+++ react-http/src/components/use-context/ex/ComponentF.js
@@ -1,26 +1,35 @@
 import React, { useContext } from 'react';
 import { UserContext, ThemeContext } from './../../../App';
 
-function ComponentF() {
-    return (
-        <div>
-            <UserContext.Consumer>
-                {
-                    (name) => {
-                        return (
-                            <ThemeContext.Consumer>
-                                {
-                                    (theme_color) => {
-                                        return <div>Component F {name}, {theme_color}</div>
-                                    }
-                                }
-                            </ThemeContext.Consumer>
-                        )
-                    }
-                }
-            </UserContext.Consumer>
-        </div>
-    )
+// ! useContext
+function ComponentF(params) {
+    const name = useContext(UserContext);
+    const theme_color = useContext(ThemeContext);
+
+    return <div>Component F {name}, {theme_color}</div>;
 }
+
+// ! useContext nested consumer
+// function ComponentF() {
+//     return (
+//         <div>
+//             <UserContext.Consumer>
+//                 {
+//                     (name) => {
+//                         return (
+//                             <ThemeContext.Consumer>
+//                                 {
+//                                     (theme_color) => {
+//                                         return <div>Component F {name}, {theme_color}</div>
+//                                     }
+//                                 }
+//                             </ThemeContext.Consumer>
+//                         )
+//                     }
+//                 }
+//             </UserContext.Consumer>
+//         </div>
+//     )
+// }
 
 export default ComponentF;
